c6a705f7c340a4260f512169d9ee294f2977ed66
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-06-01 22:48:01 -0400

    Project complete

--- Assignment2/logic.h
+++ Assignment2/logic.h
@@ -6,7 +6,6 @@ class logic
 public:
 	void setup();
 	char get_shape(int index, int index2);
-	void set_shape(int index, int index2, char c);
 	bool compare(int i, int j, int k, int l);
 	void reset_clear();
 	void correctGuess(int index, int index2);
